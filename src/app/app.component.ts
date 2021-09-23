6ebee2e3c694d6fdf34fcc5515e986c9021e8138
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-09-23 18:28:48 +0200

    Parte 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido a mi página web en Angular';
+  curso = 'Angular 7 con Spring 5';
+  alumno : string = 'Juan Carlos González Quesada';
 }
