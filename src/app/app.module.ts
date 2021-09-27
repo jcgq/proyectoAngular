57fe0af7411b77c0facde7977401e4fb53c8ba77
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-09-27 13:56:36 +0200

    CreandoFooterYHeader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
