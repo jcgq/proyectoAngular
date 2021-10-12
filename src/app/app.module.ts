32988d0fbedd4375f57824fda4a7e62ffec4ea20
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-12 18:02:16 +0200

    CRUD y Crear Cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,16 @@ import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { FormComponent } from './clientes/form.component'
+import { FormsModule } from '@angular/forms';
 
 
 const routes: Routes=[
   {path: '',redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'directivas', component: DirectivaComponent},
-  {path: 'clientes', component: ClientesComponent}
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'clientes/form', component: FormComponent}
 ]
 
 @NgModule({
@@ -25,12 +28,14 @@ const routes: Routes=[
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [ClienteService],
   bootstrap: [AppComponent]
