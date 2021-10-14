f2f62c8be5b608385f6c6cee82a1b64ae8e8f848
Juan Carlos Gonzalez Quesada <juancarlosgq@correo.ugr.es>
2021-10-14 17:10:30 +0200

    Crear y modificar clientes desde la interfaz

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {CLIENTES} from './clientes.json'
 import { Cliente } from './cliente';
 import { of,Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {map} from 'rxjs/operators';
 
 @Injectable({
@@ -12,9 +12,21 @@ export class ClienteService {
   private urlEndPoint:string = 'http://localhost:8080/api/clientes';
   constructor(private http: HttpClient) { }
 
+  private httpHeaders = new HttpHeaders({'Content-Type':'application/json'})
+
   getClientes(): Observable<Cliente[]>{
     return this.http.get<Cliente[]>(this.urlEndPoint).pipe(
       map( (response) => response as Cliente[])
     );
   }
+
+  create(cliente: Cliente):Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders});
+  }
+
+  getCliente(id): Observable<Cliente>{
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)
+  }
+
+  
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router, ActivatedRoute} from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -10,13 +13,27 @@ export class FormComponent implements OnInit {
   public cliente: Cliente = new Cliente();
   public titulo:string = "Crear Cliente";
 
-  constructor() { }
+  constructor(private clienteService: ClienteService, private router: Router, private activatedRouter: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.cargarCliente()
+  }
+
+  cargarCliente(): void{
+    this.activatedRouter.params.subscribe(param=>{
+      let id = param['id']
+      if(id){
+        this.clienteService.getCliente(id).subscribe((cliente)=>this.cliente=cliente)
+      }
+    })
   }
 
   public create(): void{
-    console.log("Clicked!")
-    console.log(this.cliente)
+    this.clienteService.create(this.cliente)
+    .subscribe(cliente =>{
+          this.router.navigate(['/clientes'])
+          swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} creado con éxito!`, 'success')
+        }
+    );
   }
 }
